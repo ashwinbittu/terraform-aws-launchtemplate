14478a70c23be6969dd4a4389ec0e7a4ffc1f0a8
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-26 22:20:47 +1000

    Add files via upload

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_launch_template" "lctemplate" {
 
   vpc_security_group_ids = [ var.securitygroup_id ]
   key_name = var.key_name
-  user_data = filebase64("${path.root}/userdata/${var.user_datascript}")
+  user_data = filebase64("${path.root}/iac/packer/userdata/${var.user_datascript}")
   ebs_optimized = true 
   #default_version = 1
   update_default_version = true 
